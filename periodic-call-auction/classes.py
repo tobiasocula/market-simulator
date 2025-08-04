011928bd94c4712d9f8f45049b978f7800217d96
Tobias Ocula <tobias.ocula@outlook.com>
2025-08-04 11:10:57 +0200

    finished pca system

--- periodic-call-auction/classes.py
+++ periodic-call-auction/classes.py
@@ -1,6 +1,7 @@
 
 from datetime import timedelta, time, datetime, date
 import numpy as np
+import sys
 import pandas as pd
 
 class PCAmarket:
@@ -60,195 +61,133 @@ class PCAmarket:
 
     def check_state(self):
         if self.order_entry_start <= self.current_time <= self.order_matching_start:
-            self.state = 1
+            self.state = 1 # order entry
         elif self.order_matching_start <= self.current_time <= self.buffer_start:
-            self.state = 2
+            self.state = 2 # order matching & trading
         else:
-            self.state = 0
+            self.state = 0 # buffer
 
     def advance_time(self, delta: timedelta):
         self.current_time = (datetime.combine(date.today(), self.current_time) + delta).time()
         self.check_state()
 
-    def fill_order(self, bid_ownerid, ask_ownerid):
-        """Updates orderbook and makes transaction"""
-        bid_order = self.order_book[self.order_book['OrderID'] == bid_ownerid]
-        ask_order = self.order_book[self.order_book['OrderID'] == ask_ownerid]
-
-        price = bid_order['Price']
-
-        print('bid order:'); print(bid_order)
-        print('ask order:'); print(ask_order)
-        print("bid_ownerid:", bid_ownerid)
-        print("ask_ownerid:", ask_ownerid)
-
-        bid_p = self.participants[self.participants['ID'] == bid_order['OwnerID']]
-        ask_p = self.participants[self.participants['ID'] == ask_order['OwnerID']]
-
-        m = min(bid_order['Volume'], ask_order['Volume'])
-        
-        ob_index_bid = self.order_book.index[self.order_book['OrderID'] == bid_ownerid]
-        ob_index_ask = self.order_book.index[self.order_book['OrderID'] == ask_ownerid]
-
-        self.order_book.loc[ob_index_bid, 'Volume'] -= m
-        self.order_book.loc[ob_index_ask, 'Volume'] -= m
-
-        print('updating balances of', bid_p.id, 'and', ask_p.id, 'to', m * bid_order.price)
-        bid_idx = self.participants.index[self.participants['ID'] == bid_order['OwnerID']]
-        ask_idx = self.participants.index[self.participants['ID'] == bid_order['OwnerID']]
-        if bid_order.volume >= ask_order.volume:
-            self.participants.loc[bid_idx, 'Balance'] -= m * price
-            self.participants.loc[bid_idx, 'AssetBalance'] += m
-            self.participants.loc[ask_idx, 'Balance'] += m * price
-            self.participants.loc[ask_idx, 'AssetBalance'] -= m
-        else:
-            self.participants.loc[bid_idx, 'Balance'] += m * price
-            self.participants.loc[bid_idx, 'AssetBalance'] -= m
-            self.participants.loc[ask_idx, 'Balance'] -= m * price
-            self.participants.loc[ask_idx, 'AssetBalance'] += m
-
-        if self.order_book.loc[ob_index_bid, 'Volume'] == 0:
-            print('deleting order')
-            self.order_book = self.order_book[self.order_book['OrderID'] != bid_ownerid]
-        if self.order_book.loc[ob_index_ask, 'Volume'] == 0:
-            print('deleting ask order')
-            self.order_book = self.order_book[self.order_book['OrderID'] != ask_ownerid]
-
     def determine_open_price(self):
         """
         -Collect all orders of the market session, ascending / descending dep. on buy/sell
         -Find matching volume
+        -Determine open price
         """
-        assert self.state == 0, AssertionError()
+        assert self.state == 1, AssertionError()
 
-        bids = self.order_book[self.order_book['Bid/Ask'] == 0][['Price', 'Volume', 'OwnerID']]
-        bids['BidVolumes'] = bids['Volume']
+        bids = self.order_book[self.order_book['Bid/Ask'] == 0][
+            ['Price', 'Volume', 'OwnerID', 'OrderID']
+            ]
+        bids['BidVolumes'] = bids['Volume'].copy()
         bids = bids.groupby('Price').agg({
                 'OwnerID': list,
                 'BidVolumes': list,
+                'OrderID': list,
                 'Volume': 'sum'
             }).reset_index()
-        bids = bids.sort_values(by=['Price'], ascending=True)
+        bids = bids.sort_values(by='Price', ascending=True)
         bids['BidCumVol'] = bids['Volume'].cumsum()
         bids = bids.drop(['Volume'], axis=1)
-        bids = bids.rename({'OwnerID': 'BidOwnerIDs'}, axis=1)
+        bids = bids.rename({'OwnerID': 'BidOwnerIDs', 'OrderID': 'BidOrderIDs'}, axis=1)
         
-        asks = self.order_book[self.order_book['Bid/Ask'] == 1][['Price', 'Volume', 'OwnerID']]
-        asks['AskVolumes'] = asks['Volume']
+        asks = self.order_book[self.order_book['Bid/Ask'] == 1][
+            ['Price', 'Volume', 'OwnerID', 'OrderID']
+            ]
+        asks['AskVolumes'] = asks['Volume'].copy()
         asks = asks.groupby('Price').agg({
                 'OwnerID': list,
                 'AskVolumes': list,
+                'OrderID': list,
                 'Volume': 'sum'
             }).reset_index()
-        asks = asks.sort_values(by=['Price'], ascending=False)
+        asks = asks.sort_values(by='Price', ascending=False)
         asks['AskCumVol'] = asks['Volume'].cumsum()
         asks = asks.drop(['Volume'], axis=1)
-        asks = asks.rename({'OwnerID': 'AskOwnerIDs'}, axis=1)
-        
+        asks = asks.rename({'OwnerID': 'AskOwnerIDs', 'OrderID': 'AskOrderIDs'}, axis=1)
+
         x = set(asks['Price']).intersection(set(bids['Price']))
         bids_f = bids[bids['Price'].isin(x)]
         asks_f = asks[asks['Price'].isin(x)]
-
         y = bids_f.merge(asks_f, on='Price').reset_index()
+
         y['MatchedVol'] = y[['AskCumVol', 'BidCumVol']].values.min(axis=1)
         self.open_price = y['Price'][np.argmax(y['MatchedVol'].values)]
         self.exec_table = y
 
     def start_trade_period(self):
+        """Executing trades and clearing order book"""
         assert self.state == 1, AssertionError("incorrect time to start trading")
-
-        def fill(askvols, bidvols, askownerids, bidownerids):
-            if (
-                not askvols or not bidvols
-                or sum(askvols) == 0 or sum(bidvols) == 0
-            ):
-                return
-            
-            for i in range(len(askvols)):
-                if askvols[i] != 0:
-                    askvols = askvols[i:]
-                    askownerids = askownerids[i:]
-                    break
-            for j in range(len(bidvols)):
-                if bidvols[j] != 0:
-                    bidvols = bidvols[j:]
-                    bidownerids = bidownerids[j:]
-                    break
-            
-            m = min(askvols[0], bidvols[0])
-            askvols[0] -= m
-            bidvols[0] -= m
-
-            self.fill_order(bidownerids[0], askownerids[0])
-            return fill(askvols, bidvols, askownerids, bidownerids)
-            
-        prev_orderbook = pd.DataFrame()
-        while not prev_orderbook.equals(self.order_book):
-            prev_orderbook = self.order_book.copy()
-            for idx, row in self.exec_table.iterrows():
-                print(row)
-                fill(
-                    row['AskVolumes'][:],
-                    row['BidVolumes'][:],
-                    row['AskOwnerIDs'][:],
-                    row['BidOwnerIDs'][:]
-                    )
-
         print(self.order_book)
-        print('balances:')
-        for p in self.participants:
-            print(p.balance)
+        for idx, row in self.exec_table.iterrows():
+            while (
+                sum(self.exec_table.loc[idx, 'AskVolumes']) > 0 and
+                sum(self.exec_table.loc[idx, 'BidVolumes']) > 0
+            ):
 
-def main():
+                for i, askvol in enumerate(self.exec_table.at[idx, 'AskVolumes']):
+                    if askvol != 0:
+                        self.exec_table.at[idx, 'AskVolumes'] = self.exec_table.at[idx, 'AskVolumes'][i:]
+                        self.exec_table.at[idx, 'AskOwnerIDs'] = self.exec_table.at[idx, 'AskOwnerIDs'][i:]
+                        self.exec_table.at[idx, 'AskOrderIDs'] = self.exec_table.at[idx, 'AskOrderIDs'][i:]
+                        break
+                for j, bidvol in enumerate(self.exec_table.at[idx, 'BidVolumes']):
+                    if bidvol != 0:
+                        self.exec_table.at[idx, 'BidVolumes'] = self.exec_table.at[idx, 'BidVolumes'][j:]
+                        self.exec_table.at[idx, 'BidOwnerIDs'] = self.exec_table.at[idx, 'BidOwnerIDs'][j:]
+                        self.exec_table.at[idx, 'BidOrderIDs'] = self.exec_table.at[idx, 'BidOrderIDs'][j:]
+                        break
+                
+                vol = min(
+                    self.exec_table.at[idx, 'AskVolumes'][0],
+                    self.exec_table.at[idx, 'BidVolumes'][0]
+                )
+                
+                price = self.exec_table.at[idx, 'Price']
+                bid_ownerid = self.exec_table.at[idx, 'BidOwnerIDs'][0]
+                ask_ownerids = self.exec_table.at[idx, 'AskOwnerIDs'][0]
+                bid_orderid = self.exec_table.at[idx, 'BidOrderIDs'][0]
+                ask_orderid = self.exec_table.at[idx, 'AskOrderIDs'][0]
 
-    n_market_participants = 100
+                self.exec_table.loc[idx, 'AskVolumes'][0] -= vol
+                self.exec_table.loc[idx, 'BidVolumes'][0] -= vol
 
-    min_number_of_trades = 10
-    max_number_of_trades = 30
+                # modify order book
+                if self.exec_table.loc[idx, 'AskVolumes'][0] == 0:
+                    self.order_book = self.order_book[
+                        self.order_book['OrderID'] != ask_orderid
+                    ]
+                else:
+                    self.order_book.loc[
+                        self.order_book['OrderID'] == ask_orderid,
+                        'Volume'
+                    ] -= vol
 
-    min_price = 7.0
-    max_price = 13.0
-    price_decimals = 2
+                if self.exec_table.loc[idx, 'BidVolumes'][0] == 0:
+                    self.order_book = self.order_book[
+                        self.order_book['OrderID'] != bid_orderid
+                    ]
+                else:
+                    self.order_book.loc[
+                        self.order_book['OrderID'] == bid_orderid,
+                        'Volume'
+                    ] -= vol
 
-    min_vol = 100
-    max_vol = 300
+                # handle funds
+                self.participants.loc[
+                    self.participants['ID'] == bid_ownerid, 'Balance'
+                ] -= vol * price
+                self.participants.loc[
+                    self.participants['ID'] == bid_ownerid, 'AssetBalance'
+                ] += vol
 
-    # initializes state to order-sending period
-    market = PCAmarket(
-        # just sample timestamps
-        order_entry_start=time(hour=7, minute=30, second=0),
-        order_matching_start=time(hour=9, minute=0, second=0),
-        buffer_start=time(hour=10, minute=0, second=0),
-        current_time=time(hour=7, minute=0, second=0)
-    )
-
-    # every market participant sends a random amount of orders
-    for i in range(n_market_participants):
-
-        market.add_participant(balance=10_000)
-
-        nr_of_trades = np.random.randint(min_number_of_trades, max_number_of_trades+1)
-
-        buy_pct = np.random.normal(0.5, 0.1)
-
-        nr_of_buys = int(nr_of_trades * buy_pct)
-        nr_of_sells = nr_of_trades - nr_of_buys
-
-        buy_prices = np.round(np.random.uniform(min_price, max_price, size=nr_of_buys), decimals=price_decimals)
-        sell_prices = np.round(np.random.uniform(min_price, max_price, size=nr_of_sells), decimals=price_decimals)
-        buy_volumes = np.round(np.random.uniform(min_vol, max_vol, size=nr_of_buys))
-        sell_volumes = np.round(np.random.uniform(min_vol, max_vol, size=nr_of_sells))
-
-        for bv, bp in zip(buy_volumes, buy_prices):
-            market.add_order(bp, bv, True, time(hour=7, minute=0, second=0), i)
-        for sv, sp in zip(sell_volumes, sell_prices):
-            market.add_order(bp, bv, False, time(hour=7, minute=0, second=0), i)
-
-    # begin trading period
-    market.determine_open_price() # determines open price of the entire trading session
-    market.advance_time(timedelta(hours=2))
-    market.start_trade_period()
+                self.participants.loc[
+                    self.participants['ID'] == ask_ownerids, 'Balance'
+                ] += vol * price
+                self.participants.loc[
+                    self.participants['ID'] == ask_ownerids, 'AssetBalance'
+                ] -= vol
         
-
-if __name__ == '__main__':
-    main()
